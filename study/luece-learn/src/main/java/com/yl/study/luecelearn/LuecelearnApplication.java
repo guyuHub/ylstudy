9d71bb6f9f01de3e498152b2ade4e7c61596b2dd
Good <Good@DESKTOP-6IGMOU5>
2018-03-22 00:24:40 +0800

    初始化

---
+++ study/luece-learn/src/main/java/com/yl/study/luecelearn/LuecelearnApplication.java
@@ -0,0 +1,12 @@
+package com.yl.study.luecelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuecelearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuecelearnApplication.class, args);
+	}
+}
---
+++ study/luece-learn/src/test/java/com/yl/study/luecelearn/LuecelearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yl.study.luecelearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LuecelearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
